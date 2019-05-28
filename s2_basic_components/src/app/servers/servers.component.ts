f4323fe547e2129f8e6faf1c8bb1a9e19c4469b3
Casares <cjcasares@gmail.com>
2019-05-28 20:58:09 +0200

    s2_26_property_binding_end
    Playing with property binding

--- s2_basic_components/src/app/servers/servers.component.ts
+++ s2_basic_components/src/app/servers/servers.component.ts
@@ -6,9 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  private serversAllowed: boolean;
+  constructor() {
+    this.serversAllowed = false;
+    setTimeout(() => {
+      this.toggleAllowStatus();
+    }, 3000);
+  }
 
-  constructor() { }
-
+  public toggleAllowStatus () {
+    if (this.serversAllowed) {
+      this.serversAllowed = false;
+    } else {
+      this.serversAllowed = true;
+    }
+  }
+  public getAllowedStatus () {
+    return this.getAllowedStatus;
+  }
   ngOnInit() {
   }
 
